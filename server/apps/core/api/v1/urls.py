dab948a1e327355b9401d713ad6246277e9ca292
meowsl <vlafrank@ya.ru>
2024-03-11 10:50:01 +0300

    fix: api config

--- server/apps/core/api/v1/urls.py
+++ server/apps/core/api/v1/urls.py
@@ -4,4 +4,6 @@ app_name = "v1"
 
 urlpatterns = [
     path("auth/", include("apps.api.auth.v1")),
+    path("company/", include("apps.api.company.v1")),
+    path("employees/", include("apps.api.employees.v1")),
 ]
